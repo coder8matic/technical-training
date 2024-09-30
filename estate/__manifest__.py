4075ddf17180811b1134dbf54daa442c27d5b822
Matic <matic@suncontract.org>
2024-09-30 13:03:10 +0200

    fix 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies
+    "depends": ["base"],  # dependencies 
     "data": [
         "security/ir.model.access.csv"
     ],
