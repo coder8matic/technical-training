275c202c02f9f0e9ec54a69658bd9ee6edf9bb2d
Matic <matic@suncontract.org>
2024-10-01 22:50:33 +0200

    add valid date for offer as compute number

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import datetime
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -13,5 +14,17 @@ class EstatePropertyOffer(models.Model):
     ], string='Status', 
        copy=False
     )
+    
+    # computed field
+    date_deadline = fields.Datetime(string='Date')
+    @api.depends("date_deadline")
+    def _compute_date_deadline(self):
+        for date in self:
+            date.date_deadline = datetime.datetime.now() + datetime.timedelta(days=7)
+    def _inverse_total(self):
+        for date in self:
+            date.date_deadline = datetime.datetime.now() #+ datetime.timedelta(days=7)
+
+    # Relations
     partner_id = fields.Many2one('res.partner', string='Partner', required=True) # Equivalent to integer
     
