a5875fdd4dc8b2eeaac96f4185a70b318cd044c0
Matic <matic@suncontract.org>
2024-10-01 14:15:22 +0200

    fix - 4

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,6 +4,7 @@ class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Property Offer'
 
+    property_id = fields.Many2one('estate.property', string='Property ID', required=True) # Equivalent to integer
     price = fields.Float(string='Price') # Equivalent to double precision
     status = fields.Selection([
         ('accepted', 'Accepted'),
@@ -11,5 +12,4 @@ class EstatePropertyOffer(models.Model):
     ], string='Status', 
        copy=False
     )
-    property_id = fields.Many2one('estate.property', string='Property ID', required=True) # Equivalent to integer
     partner_id = fields.Many2one('res.partner', string='Partner', required=True) # Equivalent to integer
