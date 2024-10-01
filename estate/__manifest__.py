2a0923c4ce30be867a4868714c7ba45b9ff2b75a
Matic <matic@suncontract.org>
2024-10-01 11:24:15 +0200

    test commit

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
+        "views/estate_property_offer_views.xml",
         "views/estate_menus.xml",
         "forms/estate_property_form.xml",
         "search/estate_property_search.xml",
